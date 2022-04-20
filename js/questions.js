934a877693ae43aedc03107e0666e1391df1ea3e
Atila BRz <0atila.brz0@gmail.com>
2022-04-20 12:44:01 -0300

    gamIHC

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,57 @@
 let questions = [
     {
     numb: 1,
-    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
-    answer: "Interface",
+    question: "Bruce encontrou dificuldade para usar uma interface, ao procurar ajuda não achou nenhuma documentação ou dicas. Qual heurística foi violada?",
+    answer: "Ajuda e Documentação",
     options: [
-      "Interface",
-      "Interação",
-      "Interatividade",
-      "Interoperabilidade"
+      "Projeto estético e minimalista",
+      "Flexibilidade e eficiência de uso",
+      "Ajuda e Documentação",
+      "Visibilidade do status do sistema"
     ]
   },
     {
     numb: 2,
-    question: "'A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
-    answer: "Interface",
+    question: "Joaquin está limitado em sua navegação pela interface. Não consegue desfaze ações, refazer ou sair de locais indesejados. Qual heurística foi violada?",
+    answer: "Controle e liberdade do usuário",
     options: [
-      "Interface",
-      "Interação",
-      "Interatividade",
-      "Interoperabilidade"
+      "Flexibilidade",
+      "Controle e liberdade do usuário",
+      "Consistência e padronização",
+      "Prevenção de erros"
     ]
   },
     {
     numb: 3,
-    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
-    answer: "Interface",
+    question: "Martha está acessando um formulário extenso e após alguns minutos não consegue visualizar em qual etapa está e quantas há. Qual heurística foi violada?",
+    answer: "Visbilidade do status do sistema",
     options: [
-      "Interface",
-      "Interação",
-      "Interatividade",
-      "Interoperabilidade"
+      "Design estético e minimalista",
+      "Ajuda e documentação",
+      "Consistência e padrões",
+      "Visbilidade do status do sistema"
     ]
   },
     {
     numb: 4,
-    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
-    answer: "Interface",
+    question: "Clark percebe que o site contém botões em páginas diferentes, porém com a mesma funcionalidade, que não seguem um padrão de cores. Qual heurística foi violada?",
+    answer: "Consistência e padronização",
     options: [
-      "Interface",
-      "Interação",
-      "Interatividade",
-      "Interoperabilidade"
+      "Flexibilidade e eficiência de uso ",
+      "Controle e liberdade do usuário ",
+      "Consistência e padronização",
+      "Ajuda e documentação "
     ]
   },
     {
     numb: 5,
-    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
-    answer: "Interface",
+    question: "Barry, ao acessa um telejornal, se assuta com a quantidade de anúcios presentes na tela, além dos textos enormes e muitas imagens. Qual heurística foi violada?",
+    answer: "Design estético e minimalista",
     options: [
-      "Interface",
-      "Interação",
-      "Interatividade",
-      "Interoperabilidade"
+      "Design estético e minimalista",
+      "Consistência e padrões",
+      "Ajuda e documentação",
+      "Flexibilidade"
     ]
   },
 ];
