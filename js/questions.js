72e02e8d9ad1f858b986532a7b709dd5ef23655f
Atila BRz <0atila.brz0@gmail.com>
2022-04-20 01:02:10 -0300

    gamIHC

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
+    answer: "Interface",
+    options: [
+      "Interface",
+      "Interação",
+      "Interatividade",
+      "Interoperabilidade"
+    ]
+  },
+    {
+    numb: 2,
+    question: "'A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
+    answer: "Interface",
+    options: [
+      "Interface",
+      "Interação",
+      "Interatividade",
+      "Interoperabilidade"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
+    answer: "Interface",
+    options: [
+      "Interface",
+      "Interação",
+      "Interatividade",
+      "Interoperabilidade"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
+    answer: "Interface",
+    options: [
+      "Interface",
+      "Interação",
+      "Interatividade",
+      "Interoperabilidade"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A superfície de contato que reflete propriedades físicas das partes que interagem e suas funções é a:",
+    answer: "Interface",
+    options: [
+      "Interface",
+      "Interação",
+      "Interatividade",
+      "Interoperabilidade"
+    ]
+  },
+];
